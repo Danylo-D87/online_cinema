4af3e0e3bc111d375535eb78f23c9ff213ef89da
Danylo-D87 <quaestor88@gmail.com>
2025-06-17 13:21:34 +0300

    added migrations

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,3 +3,4 @@ from .user import User
 from .activation_token import ActivationToken
 from .refresh_token import RefreshToken
 from .genre import Genre
+from .movie import Movie, movie_genre_association
