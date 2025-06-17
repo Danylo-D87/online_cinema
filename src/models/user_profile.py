b07eda32bd340f4b1b33135c469a063aebf53a7c
Danylo-D87 <quaestor88@gmail.com>
2025-06-17 13:45:32 +0300

    added migration all tables

--- src/models/user_profile.py
+++ src/models/user_profile.py
@@ -5,7 +5,7 @@ from sqlalchemy import (
     String,
     Date,
     ForeignKey,
-    Text,
+    Text, Enum,
 )
 from sqlalchemy.orm import relationship
 from src.database.base import Base
@@ -24,11 +24,12 @@ class UserProfile(Base):
     first_name = Column(String(50), nullable=True)
     last_name = Column(String(50), nullable=True)
     avatar = Column(String, nullable=True)
-    gender = Column(enum.Enum(GenderEnum), nullable=True)
+    gender = Column(Enum(GenderEnum), nullable=True)
     date_of_birth = Column(Date, nullable=True)
     info = Column(Text, nullable=True)
 
     user = relationship("User", back_populates="profile")
+
 
     def __repr__(self):
         return f"<UserProfile(user_id={self.user_id}, first_name='{self.first_name}')>"
