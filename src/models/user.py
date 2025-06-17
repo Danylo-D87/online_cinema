dad1b04e2b865591553dd564e50fc9c0f9e8525a
Danylo-D87 <quaestor88@gmail.com>
2025-06-17 13:29:16 +0300

    added password_reset_tokens model

--- src/models/user.py
+++ src/models/user.py
@@ -38,6 +38,12 @@ class User(Base):
         uselist=False,
         cascade="all, delete-orphan",
     )
+    password_reset_token = relationship(
+        "PasswordResetToken",
+        back_populates="user",
+        uselist=False,
+        cascade="all, delete-orphan",
+    )
 
     def __repr__(self):
         return f"<User(email='{self.email}', is_active={self.is_active})>"
