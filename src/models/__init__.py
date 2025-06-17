a7015d85bf86a7f345aa98c35fbd459a393bd9b8
Danylo-D87 <quaestor88@gmail.com>
2025-06-17 13:07:38 +0300

    added imports to models/__init__

---
+++ src/models/__init__.py
@@ -0,0 +1,7 @@
+from .user_group import UserGroup, UserGroupEnum
+from .user import User
+from .activation_token import ActivationToken
+from .refresh_token import RefreshToken
+from .genre import Genre
+
+from src.database.base import Base
